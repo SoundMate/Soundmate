9f36eac3ee219f4734aefb63e16a7d9f587cf7e5
LorenzoPantano <49166985+LorenzoPantano@users.noreply.github.com>
2020-10-31 16:50:57 +0100

    Info Page

--- src/main/java/it/soundmate/controller/RegisterController.java
+++ src/main/java/it/soundmate/controller/RegisterController.java
@@ -47,6 +47,9 @@ public class RegisterController {
             stage.setScene(scene);
             stage.show();
         }
+        else {
+            //TODO: Test Sonarcloud Code Smell
+        }
     }
 
     @FXML
